c303a8f5e6dffae95d56dbf4757ec1546ed29b8d
Arthur Cavalcante <linsarthur02@gmail.com>
2025-11-02 12:13:37 -0300

    added a standart player video with the main text i will improve the video player as soon i get a better option

---
+++ src/components/VideoPresentation.jsx
@@ -0,0 +1,30 @@
+import storageVideo from "../assets/storageVideo.mp4";
+
+const VideoPresentation = () => {
+  return (
+    <>
+      <div className="flex justify-between px-50 py-10 gap-20 items-center w-full">
+        <iframe
+          src={storageVideo}
+          frameborder="0"
+          width="400"
+          height="400"
+          title="Storage presentation"
+          allowFullScreen
+        
+        />
+
+        <div>
+          <h3 className="font-bold text-4xl">Veja como funciona o sistema da Stock Master</h3>
+          <p className="font-light text-[18px] text-[#66768C] mt-[30px]">
+            Simplifique a gestão do seu estoque com o StockMaster! Tenha
+            controle total das suas vendas, otimize processos e aumente seus
+            lucros.
+          </p>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default VideoPresentation;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,12 @@
 import Banner from "../components/Banner";
 import NavBar from "../components/NavBar";
+import VideoPresentation from "../components/VideoPresentation";
 
 const Home = () => {
     return <>
     <NavBar/>
     <Banner/>
+    <VideoPresentation/>
     </>;
 }
  
