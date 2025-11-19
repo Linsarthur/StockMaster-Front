216da6fcbb6fab2e7514180b0b3cfaebc12c70f8
Arthur cavalcante <linsarthur02@gmail.com>
2025-11-19 18:36:40 -0300

    fixing logins rout

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -3,7 +3,7 @@ const Button = ({ text, style }) => {
     <div>
       <div
         className={`
-          py-1 px-2 text-[10px] hidden
+          py-1 px-2 text-[10px] 
           xl:px-[26px] xl:py-1.5 ${style} hover:bg-[#007afc] rounded-full xl:text-[16px] xl:font-bold cursor-pointer xl:block`}
       >
         {text}
