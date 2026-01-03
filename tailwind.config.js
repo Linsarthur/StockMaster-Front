2e5494adcad2951b7f281053743a388f3a322fe1
Arthur Cavalcante <linsarthur02@gmail.com>
2026-01-03 13:10:56 -0300

    side menu almost ready

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,7 +7,11 @@ export default defineConfig({
         './src/**/*.{js,ts,jsx,tsx}',
     ],
     theme: {
-        extend: {},
+        extend: {
+            boxShadow: {
+                insetDefault: 'inset 0px 4px 4px 0px rgba(0,0,0,0.25)',
+            },
+        },
     },
     plugins: [],
 })
