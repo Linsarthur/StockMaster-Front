9b1e7b920d547fd53005cbb935f70285b6fffb56
Arthur Cavalcante <linsarthur02@gmail.com>
2026-01-03 12:17:43 -0300

    users data displayed in the sidebar menu

--- src/pages/DashBoard.jsx
+++ src/pages/DashBoard.jsx
@@ -1,25 +1,22 @@
 import NavBar from "../components/NavBar";
-
+import avatarBatman from "../assets/avatar-batman.png"
+import { useContext } from "react";
+import { LoginContext } from "../context/loginContext";
 const DashBoard = () => {
+  const {usuario} = useContext(LoginContext)
   return (
     <>
       <NavBar />
-      <div className="w-[25%] h-screen bg-[#DBEDFF] flex ">
-        <div>
-          <div className="flex items-center bg-linear-to-t from-white/60 to-white/5 ">
-            <div>
-              <box-icon
-                name="user-circle"
-                type="solid"
-                color="#1a2371"
-              ></box-icon>
-            </div>
+      <div className="w-[25%] h-screen bg-[#DBEDFF] flex">
+        <div className="flex h-fit w-[80%] mx-8 mt-[60px]  pb-5 pl-5 gap-5 rounded bg-linear-to-t from-white to-white/5 ">
+          <div className=" flex items-center">
+            <img src={avatarBatman} alt="" />
+          </div>
 
-            <div>
-              <p>Nome do user</p>
-              <p>Email</p>
-              <p>Perfil: tipo</p>
-            </div>
+          <div className="text-[#1C274C]">
+            <p className="font-bold text-[20px] leading-10 mb-2">{usuario.user_name}</p>
+            <p className="font-light text-[16px] leading-[100%] mb-2">{usuario.user_email}</p>
+            <p className="font-light text-[14px] leading-[100%]"><span className="font-semibold ">Perfil:</span> tipo</p>
           </div>
         </div>
       </div>
